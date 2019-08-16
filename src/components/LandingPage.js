ac9d0623a6b3d8c118d52b441c27d014171a8df0
Gabriel Parke <gparke@innovu.com>
2019-08-16 11:01:34 -0400

    Add in landing page text

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,6 +5,10 @@ import '../styles/landingpage.scss';
 const LandingPage = () => {
 	return (
 		<div>
+			<h3>Welcome to Damage Tracker.</h3>
+			<p>Where either dungeon masters or players can keep track of the health of enemies.</p>
+			<p>This is a continually developed personal project with more features that will get added in whenever I have time.</p>
+			<p>To speek up the process, feel free to donate below. If the button isn't there I haven't built it yet.</p>
 			<Link className="link-button" to="/dm">Master of Puppets</Link>
 			<Link className="link-button" to="/player">Puppets</Link>
 		</div>
